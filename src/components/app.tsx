b5dd5db728dfb7667364c9cbf83af96afa2ba6d4
Jairo <jairo.viciana@accenture.com>
2018-02-08 17:32:54 +0100

    async actions

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,7 +1,10 @@
-export const App = (state, actions) => (
+import { Actions } from "../state/actions";
+
+export const App = (state, actions: Actions) => (
   <div>
     <h1>{state.count}</h1>
     <button onclick={() => actions.down(2)}>-</button>
     <button onclick={() => actions.up(2)}>+</button>
+    <button onclick={() => actions.upAsync(2)}>async +</button>
   </div>
 );
--- src/state/actions.ts
+++ src/state/actions.ts
@@ -1,4 +1,12 @@
+export type Actions = {
+  down: Function;
+  up: Function;
+  upAsync: Function;
+};
+
 export const actions = {
   down: value => state => ({ count: state.count - value }),
-  up: value => state => ({ count: state.count + value })
+  up: value => state => ({ count: state.count + value }),
+  upAsync: value => async (state, actions: Actions) =>
+    setTimeout(() => actions.up(value), 1000)
 };
